8c40b915e7a3e77057b839192c49228c1fd0a005
Matvey Kolosov <118886359+MKLSV@users.noreply.github.com>
2023-03-23 21:11:09 +0200

    API

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
